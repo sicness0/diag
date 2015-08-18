936aea6e26a22c457cfd5939bf41fad4422d3139
unknown <matt@VOSTRO460.yes-it.local>
2015-08-18 12:17:48 +1000

    Added Test comment for version

--- BVSCDiagnostics/Program.cs
+++ BVSCDiagnostics/Program.cs
@@ -18,5 +18,6 @@ namespace BVSCDiagnostics
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
         }
+	/// Added test comment
     }
 }
